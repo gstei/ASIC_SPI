4b117649eaafe36e1afd0d23a060b7e8fd8b29ee
Matthias Meyer <matthias.meyer@simes01.ost.ch>
2023-01-13 15:03:28 +0100

    modified naming

--- controller/sources/ctrl.vhd
+++ controller/sources/ctrl.vhd
@@ -21,36 +21,36 @@ end ctrl;
 architecture rtl of ctrl is
     type state is (S0, S1, S2, S3);
     signal c_st, n_st : state;
-    --define ram
+    --define ctrl_reg
     type t_register is array (0 to 2**AW-1) of
     std_logic_vector(DW-1 downto 0);
-    signal ram : t_register := ((others=> (others=>'0')));
+    signal ctrl_reg : t_register := ((others=> (others=>'0')));
 begin
     -- memorizing process
     p_seq: process (rst, clk)
     begin
         if rst = '1' then
             c_st <= S0;
---            ram <= (others=> (others=>'0'));
+--            ctrl_reg <= (others=> (others=>'0'));
         elsif rising_edge(clk) then
             c_st <= n_st;
         end if;
     end process;
     -- memoryless process
-    p_com: process (o_busy,i_data_from_spi, ram, c_st)
+    p_com: process (o_busy,i_data_from_spi, ctrl_reg, c_st)
     begin
     -- default assignments
     n_st <= c_st; -- remain in current state
     --data to spi is value of the register with the address stored in register zero
-    o_data_to_spi <= ram(to_integer(unsigned(ram(0)(3 downto 1))));
+    o_data_to_spi <= ctrl_reg(to_integer(unsigned(ctrl_reg(0)(3 downto 1))));
     -- specific assignments
     case c_st is
         when S0 =>
             if o_busy = '1' then n_st <= S1;
             end if;
             --if write is set to one and register address is larger than one then write register.
-            if ram(0)(0) = '1' and unsigned(ram(0)(3 downto 1)) > to_unsigned(1,3) then
-                ram(to_integer(unsigned(ram(0)(3 downto 1)))) <= i_data_from_spi;
+            if ctrl_reg(0)(0) = '1' and unsigned(ctrl_reg(0)(3 downto 1)) > to_unsigned(1,3) then
+                ctrl_reg(to_integer(unsigned(ctrl_reg(0)(3 downto 1)))) <= i_data_from_spi;
             end if;
         when S1 =>
             if o_busy = '0' then
@@ -59,7 +59,7 @@ begin
         when S2 =>
             if o_busy = '1' then n_st <= S3;
             end if;
-            ram(0) <= i_data_from_spi;
+            ctrl_reg(0) <= i_data_from_spi;
         when S3 =>
             if o_busy = '0' then n_st <= S0;
             end if;
